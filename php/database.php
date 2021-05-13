d05b93d71bf443f7562cab203e43bdd38d9d0568
Richard Lastrucci <lastrucci63@gmail.com>
2021-05-13 10:33:22 +0200

    we tried

--- php/database.php
+++ php/database.php
@@ -19,5 +19,22 @@
         public function __destruct() {
 
         }
+
+        public function findUser($id) {
+            $sql = "Select id_no from person where id_no=$id";
+            if($conn->query($sql) === true)
+                echo "New record created successfully";
+            else
+                echo "Error: " . $sql . "<br>" . $conn->error;
+        }
+
+        // returns a boolean whether the password matches the stored password
+        public function validateUserPass($id, $pass) {
+            $sql = "SELECT * FROM PERSON WHERE id_no = $id AND password = $pass"
+            if($conn->query($sql) === true)
+                echo "Password and id checked against parametres";
+            else
+                echo "Error: " . $sql . "<br>" . $conn->error;
+        }
     }
 ?>
