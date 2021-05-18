68e2a467af15703c9d4469c5659089277b7c488f
Richard Lastrucci <lastrucci63@gmail.com>
2021-05-18 15:53:36 +0200

    updated database.php and fixed syntax errors

--- php/database.php
+++ php/database.php
@@ -12,11 +12,18 @@
             return $instance;
         }
 
-        private function __construct() {
-            $conn = new mysqli("", "", "");
-            if($conn === false)
-                die("Error: Failed to connect " . $conn->connect_error);
-            $conn->select_db("");
+        public function __construct() {
+            $DBusername = "u20430168";
+            $DBpassword = "RHBBQV2AGCFKHVN4DCEMTXNMSE7YCQWE";
+            $DBhost = "wheatley.cs.up.ac.za";
+            $DBname = "u20430168_election_system";
+
+            $this->conn = new mysqli($DBhost, $DBusername, $DBpassword, $DBname);
+            if($this->conn === false)
+                die("Error: Failed to connect " . $this->conn->connect_error);
+            $this->conn->select_db("");
+       
+         
         }
 
         public function __destruct() {
@@ -25,34 +32,43 @@
 
         public function findUser($id) {
             $sql = "Select id_no from person where id_no=$id";
-            if($conn->query($sql) === true)
+            if($this->conn->query($sql) === true)
                 echo "New record created successfully";
             else
-                echo "Error: " . $sql . "<br>" . $conn->error;
+                echo "Error: " . $sql . "<br>" . $this->conn->error;
         }
 
         // returns a boolean whether the password matches the stored password
         public function validateUserPass($id, $pass) {
             $sql = "SELECT * FROM PERSON WHERE id_no = $id AND password = $pass";
-            if($conn->query($sql) === true)
+            if($this->conn->query($sql) === true)
                 echo "Password and id checked against parametres";
             else
-                echo "Error: " . $sql . "<br>" . $conn->error;
+                echo "Error: " . $sql . "<br>" . $this->conn->error;
         }
-        // returns the result
-        public function findUser($id) {
-            $sql = "Select * from person where id_no=$id";
-            $result = $conn->query($sql);
 
-            return $result;
-        }
+        // returns the result
+        // public function findUser($id) {
+        //     $sql = "Select * from person where id_no=$id";
+        //     $result = $this->conn->query($sql);
+
+        //     return $result;
+        // }
 
         // returns a bool
         public function checkUserExists($id) {
             $sql = "Select id_no from person where id_no=$id";
-            $result = $conn->query($sql);
+            $result = $this->conn->query($sql);
 
             return ($result->num_rows > 0);
         }
+
+        
     }
+
+
+    $db = new Database();
+    $db->__construct();
+    
+    
 ?>
