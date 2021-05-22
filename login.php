427f7c25fdea4680c587430040bad7e2e7b6e3a0
Cara Grobler <u20478144@tuks.co.za>
2021-05-22 22:06:49 +0200

    New Sign Up and Login

--- login.php
+++ login.php
@@ -8,7 +8,28 @@
     <body>
         <?php include_once("php/header.php") ?>
 
-        <!--ACTUAL LOGGING PAGE-->
+        <div class="center1">
+            <form method="post">
+                <div class="imgcontainer">
+                    <img src="img/login.png" alt="Avatar" class="avatar">
+                </div>
+
+                <div class="container">
+                    <label for="uname"><b>Username</b></label>
+                    <input type="text" placeholder="Enter Username" name="uname" required>
+
+                    <label for="psw"><b>Password</b></label>
+                    <input type="password" placeholder="Enter Password" name="psw" required>
+                        
+                    <button type="submit">Login</button>
+                </div>
+
+                <div class="container" style="background-color:#f1f1f1">
+                    <span class="signup">Not Registered? <a href="signup.php">SignUp</a></span>
+                </div>
+            </form>
+        </div>
+        <!--ACTUAL LOGGING PAGE
         <div class="center1">
             <h1>Login</h1>
             <form method="post">
@@ -29,6 +50,6 @@
 
             </form>
 
-        </div>
+        </div>-->
     </body>
 </html>
--- signup.php
+++ signup.php
@@ -8,7 +8,50 @@
     <body>
         <?php include_once("php/header.php") ?>
 
-        <!--SIGN UP REG PAGE-->
+        <div class="center">
+            <form method="post">
+                <div class="imgcontainer">
+                    <img src="img/signup.png" alt="Avatar" class="avatar">
+                </div>
+
+                <div class="container">
+                    <label for="id"><b>SA ID Number</b></label>
+                    <input type="text" placeholder="Enter SA ID Number" name="id" required>
+
+                    <label for="name"><b>Name</b></label>
+                    <input type="text" placeholder="Enter Name" name="name" required>
+
+                    <label for="middle_name"><b>Middle Name/s</b></label>
+                    <input type="text" placeholder="Enter Middle Name/s" name="middle_name">
+
+                    <label for="surname"><b>Surname</b></label>
+                    <input type="text" placeholder="Enter Surname" name="surname" required>
+
+                    <label for="cellphone"><b>Cellphone</b></label>
+                    <input type="text" placeholder="Enter Cellphone" name="cellphone" required>
+
+                    <label for="email"><b>Email</b></label>
+                    <input type="text" placeholder="Enter Email" name="email">
+
+                    <label for="address"><b>Address</b></label>
+                    <input type="text" placeholder="Enter Address" name="address" required>
+
+                    <label for="psw"><b>Password</b></label>
+                    <input type="password" placeholder="Enter Password" name="psw" required>
+                        
+                    <button type="submit">Sign Up</button>
+                </div>
+
+                <div class="container" style="background-color:#f1f1f1">
+                    <span class="signup">Already Registered? <a href="login.php">Login</a></span>
+                </div>
+            </form>
+        </div>
+
+
+
+
+        <!--SIGN UP REG PAGE
         <div class="center" id="signup">
             <h1>Register</h1>
             <form method="post">
@@ -23,7 +66,7 @@
                     <label>Name</label>
                 </div>
                 <div class="txt_field">
-                    <input type="text" required>
+                    <input type="text">
                     <span></span>
                     <label>Middle-Names</label>
                 </div>
@@ -59,6 +102,6 @@
 
             </form>
 
-        </div>
+        </div>-->
     </body>
 </html>
