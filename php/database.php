d2215aa045a56dd71996f02e1be9bdd58ef25690
Richard Lastrucci <lastrucci63@gmail.com>
2021-05-26 08:53:05 +0200

    ??

--- php/database.php
+++ php/database.php
@@ -31,24 +31,23 @@
             $stmt = $this->conn->prepare("Select id_no from person where id_no=?");
             $stmt->bind_param("s", $id);
             $stmt->execute();
-            
+
             $stmt->bind_result($data);
-            echo '<script> console.log('.$id.')</script>';
             $stmt->fetch();
 
-            return $stmt->num_rows() > 0;
+            return ($data === $id);
         }
 
         // returns true if email is found
         public function checkEmailExists($email) {
-            $stmt = $this->conn->prepare("Select id_no from person where email=?");
+            $stmt = $this->conn->prepare("Select email from person where email=?");
             $stmt->bind_param("s", $email);
             $stmt->execute();
 
             $stmt->bind_result($data);
             $stmt->fetch();
-
-            return $stmt->num_rows() > 0;
+            // die($data);
+            return ($data === $email);
         }
 
         // returns a bool
@@ -63,15 +62,22 @@
             return $stmt->num_rows() > 0;
         }
 
+        public function checkCellExists($cell) {
+            $stmt = $this->conn->prepare("Select cell from person where cell=?");
+            $stmt->bind_param("s", $cell);
+            $stmt->execute();
+
+            $stmt->bind_result($data);
+            $stmt->fetch();
+
+            return ($data === $cell);
+        }
         
         public function addUser($id, $name, $mname, $surname, $cell, $email, $addr, $pass, $ward_id) {
-            $stmt = $this->conn->prepare("insert into person values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+            $stmt = $this->conn->prepare("insert into person values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, 0)");
             $voted = 0;
-            $verified = 0;
             settype($ward_id, "integer");
-            // if ($stmt===false) 
-            //     die($this->conn->error);
-            $stmt->bind_param("ssssssssiii", $id, $name, $mname, $surname, $cell, $email, $addr, $pass, $ward_id, $voted, $verified);
+            $stmt->bind_param("ssssssssii", $id, $name, $mname, $surname, $cell, $email, $addr, $pass, $ward_id, $voted);
             if ($stmt->execute())
             echo 'success';
             else
@@ -88,19 +94,7 @@
             $stmt->bind_result($data);
             $stmt->fetch();
 
-            return $stmt->num_rows() > 0;
-        }
-        
-        public function checkCellExists($cell) {
-            $sql = "Select id from person where cell=$cell";
-            $stmt = $this->conn->prepare("Select id from person where cell=?");
-            $stmt->bind_param("s", $cell);
-            $stmt->execute();
-
-            $stmt->bind_result($data);
-            $stmt->fetch();
-
-            return $stmt->num_rows() > 0;
+            return ($data == $ward);
         }
         
         // its in the name, also returns a bool
@@ -180,7 +174,6 @@
             } 
         
             $sql->close();
-
         }
 
         public function getUserDetailsAll($id) {
@@ -197,6 +190,135 @@
                 die("Error: Failed to connect to database");
 
             return array("f_name" => $fname, "m_name" => $mname, "l_name" => $lname, "cell" => $cell, "email" => $email, "address" => $address);
+        }
+
+        public function getCandidates($wardID) {
+            $stmt = $this->conn->prepare("Select candidate.id_no, f_name, l_name from person inner join candidate on person.id_no = candidate.id_no where candidate.ward_id = ?");
+            $stmt->bind_param("i", $wardID);
+            if (!$stmt->execute())
+                die("Error: Failed to connect to database");
+            $stmt->bind_result($id, $fname, $lname);
+
+            $candidates = array();
+            $i = 0;
+            while ($stmt->fetch()) {
+                $candidates[$i] = array("id" => $id, "fname" => $fname, "lname" => $lname);
+                $i++;
+            }
+
+            return $candidates;
+        }
+
+        public function getParties($wardID, $type) {
+            $sql = "";
+            if ($type === 'l')
+                $sql = "Select has.p_id, p_name, abbr from political_party inner join has on political_party.p_id = has.p_id where has.ward_id = ? and isDist <> 0";
+            else if ($type === 'd')
+            $sql = "Select has.p_id, p_name, abbr from political_party inner join has on political_party.p_id = has.p_id where has.ward_id = ? and isDist = 0";
+            else if ($type === 'a')
+                $sql = "Select has.p_id, p_name, abbr from political_party inner join has on political_party.p_id = has.p_id where has.ward_id = ?";
+        
+            $stmt = $this->conn->prepare($sql);
+            $stmt->bind_param("i", $wardID);
+            if (!$stmt->execute())
+                die("Error: Failed to connect to database");
+            $stmt->bind_result($p_id, $name, $abb);
+
+            $parties = array();
+            $i = 0;
+            while ($stmt->fetch()) {
+                $parties[$i] = array("pID" => $p_id, "name" => $name, "abb" => $abb);
+                $i++;
+            }
+
+            return $parties;
+        }
+
+        public function isLocal($wardID) {
+            $stmt = $this->conn->prepare("Select dist_id from municipality inner join ward on ward.mun_id = municipality.mun_id where ward_id = ?");
+            $stmt->bind_param("i", $wardID);
+            if (!$stmt->execute())
+                die("Error: Failed to connect to database");
+            $stmt->bind_result($distID);
+            $stmt->fetch();
+            return $distID != 0;
+        }
+
+        public function addPartyVote($pID, $wardID) {
+            $stmt = $this->conn->prepare("update has set votes=votes+1 where p_id=? and ward_id=?");
+            $stmt->bind_param("ii", $pID, $wardID);
+            if (!$stmt->execute())
+                die("Error: Failed to connect to database");
+        }
+
+        public function addCanVote($canID, $wardID) {
+            $stmt = $this->conn->prepare("update candidate set votes=votes+1 where id_no=? and ward_id=?");
+            $stmt->bind_param("ii", $canID, $wardID);
+            if (!$stmt->execute())
+                die("Error: Failed to connect to database");
+        }
+
+        public function setVoted($id) {
+            $stmt = $this->conn->prepare("update person set voted_flag=true where id_no=?");
+            $stmt->bind_param("i", $id);
+            if (!$stmt->execute())
+                die("Error: Failed to connect to database");
+        }
+
+        public function setVerified($id) {
+            $stmt = $this->conn->prepare("update person set verified_IEC=true where id_no=?");
+            $stmt->bind_param("i", $id);
+            if (!$stmt->execute())
+                die("Error: Failed to connect to database");
+        }
+
+        public function getUnverified() {
+            $stmt = $this->conn->prepare("Select id_no, f_name, m_name, l_name, cell, email, address from person where verified_IEC = 0");
+            if (!$stmt->execute())
+                die("Error: Failed to connect to database");
+            $stmt->bind_result($id, $fname, $mname, $lname, $cell, $email, $addr);
+
+            $people = array();
+            $i = 0;
+            while ($stmt->fetch()) {
+                $people[$i] = array(
+                    "id" => $id,
+                    "fname" => $fname,
+                    "mname" => $mname,
+                    "lname" => $lname,
+                    "cell" => $cell,
+                    "email" => $email,
+                    "addr" => $addr
+                );
+                $i++;
+            }
+
+            return $people;
+        }
+
+        public function addParty($name, $abb) {
+            $stmt = $this->conn->prepare("insert into political_party(p_name, abbr) value(?,?)");
+            $stmt->bind_param("ss", $name, $abb);
+            if (!$stmt->execute())
+                die("Error: Failed to connect to database");
+        }
+
+        public function checkPartyExists($pid) {
+            $stmt = $this->conn->prepare("Select p_id from political_party where p_id=?");
+            $stmt->bind_param("s", $pid);
+            $stmt->execute();
+
+            $stmt->bind_result($data);
+            $stmt->fetch();
+
+            return ($data === $pid);
+        }
+
+        public function addCandidate($id, $ward, $pid, $post) {
+            $stmt = $this->conn->prepare("insert into candidate value(?,?,?,0,?)");
+            $stmt->bind_param("siis", $id, $ward, $pid, $post);
+            if (!$stmt->execute())
+                die("Error: Failed to connect to database");
         }
     }
 ?>
